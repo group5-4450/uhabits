ee3294e452e1556a069f15778eb3192d8c15b7a1
Jordan MacIntyre <jordanroymax@gmail.com>
2016-10-28 18:40:51 -0400

    added a note package

---
+++ app/src/main/java/org/isoron/uhabits/activities/habits/note/CreateHabitDialog.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 Álinson Santos Xavier <isoron@gmail.com>
+ *
+ * This file is part of Loop Habit Tracker.
+ *
+ * Loop Habit Tracker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Loop Habit Tracker is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.isoron.uhabits.activities.habits.note;
+
+import com.google.auto.factory.AutoFactory;
+
+import org.isoron.uhabits.R;
+import org.isoron.uhabits.commands.Command;
+import org.isoron.uhabits.models.Frequency;
+
+@AutoFactory(allowSubclasses = true)
+public class CreateHabitDialog extends NoteDialog
+{
+    @Override
+    protected int getTitle()
+    {
+        return R.string.create_habit;
+    }
+
+    @Override
+    protected void initializeHabits()
+    {
+        modifiedHabit = modelFactory.buildHabit();
+        modifiedHabit.setFrequency(Frequency.DAILY);
+        modifiedHabit.setColor(
+            prefs.getDefaultHabitColor(modifiedHabit.getColor()));
+    }
+
+    @Override
+    protected void saveHabit()
+    {
+        Command command = appComponent
+            .getCreateHabitCommandFactory()
+            .create(habitList, modifiedHabit);
+        commandRunner.execute(command, null);
+    }
+}
